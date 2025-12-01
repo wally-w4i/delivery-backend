7d5a976a64c91fa21786f42394cbcdb9dfc371f7
Walter Gabriel Orozco <walter.gabriel.orozco@gmail.com>
2025-12-01 17:19:34 +0200

    feat: Add delivery and show deliveries

---
+++ src/main/java/com/gloomygold/delivery/controller/DeliveryController.java
@@ -0,0 +1,31 @@
+package com.gloomygold.delivery.controller;
+
+import com.gloomygold.delivery.dto.DeliveryDTO;
+import com.gloomygold.delivery.service.DeliveryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+@Controller
+@RequestMapping("/api/deliveries")
+public class DeliveryController {
+
+    @Autowired
+    private DeliveryService service;
+
+    @GetMapping
+    public ResponseEntity<List<DeliveryDTO>> getDeliveries() {
+        return ResponseEntity.ok(service.getDeliveries());
+    }
+
+    @PostMapping
+    public ResponseEntity<DeliveryDTO> createDelivery(@RequestBody DeliveryDTO d) {
+        DeliveryDTO created = service.createDelivery(d);
+        return ResponseEntity.created(URI.create("/api/deliveries")).body(created);
+    }
+
+}
---
+++ src/main/java/com/gloomygold/delivery/repository/DeliveryRepository.java
@@ -0,0 +1,10 @@
+package com.gloomygold.delivery.repository;
+
+
+import com.gloomygold.delivery.model.Delivery;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DeliveryRepository extends JpaRepository<Delivery, Long> {
+}
